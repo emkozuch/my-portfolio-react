d9b4967482d6359976cf41b02761bbf9cb12930e
Marcin <emkozuch@gmail.com>
2020-01-12 20:30:44 +0100

    background image

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../Styles/Routes/Home.scss'
 
 function Home() {
     return (
-        <div>
-            
+        <div className="home-container">
+            <h1>Marcin <span>Kożuchowski</span></h1>
+            <h2>Front-end Developer</h2>
         </div>
     )
 }
