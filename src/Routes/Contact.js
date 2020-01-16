5538d4b7da85b5ddd00f5e48c1d6d39ab590f361
Marcin <emkozuch@gmail.com>
2020-01-17 00:33:15 +0100

    basic contact form

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import RouteContainer from '../Components/RouteContainer'
+import RouteTitle from '../Components/RouteTitle'
+import mail from '../images/icons/mail.svg'
+import '../Styles/Routes/Contact.scss'
 function Contact() {
     return (
-        <div>
-            
-        </div>
+        <RouteContainer>
+            <RouteTitle src={mail} title="Kontakt" />
+            <div className="contact-form-container">
+                <form>
+                    <div className="first-row">
+                        <input type="text" placeholder="Imię"></input>
+                        <input type="email" placeholder="Email"></input>
+                    </div>
+                    <div className="second-row">
+                        <textarea placeholder="Wiadomość..."></textarea>
+                        <button>Wyślij wiadomość</button>
+                    </div>
+
+
+                </form>
+            </div>
+        </RouteContainer>
     )
 }
 
